af064b8266baf6d2d1b342e2ed8fd0b2260df6d2
prakashdeep2902 <prakashdeep2902@gmail.com>
2023-03-03 18:34:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+import SimpleBottomNavigation from "./components/MainNav";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
+import { Container } from "@material-ui/core";
+
+function App() {
+  return (
+    <BrowserRouter>
+    
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
+  );
+}
+
+export default App;
